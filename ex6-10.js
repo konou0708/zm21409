def8c2da3ec79d4b52b389811f9f8e2453cc0fed
konou0708 <103973949+konou0708@users.noreply.github.com>
2022-12-09 15:50:47 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
 function imgchange2(){document.getElementById('logo').src='./image/bulbon.gif';}
-function imgchange3(){document.getElementById('logo').src='./image/';}
+function imgchange3(){document.getElementById('logo').src='./image/1611144883117.jpg';}
