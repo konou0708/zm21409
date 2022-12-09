fd6863f3160527a1839dadfd752776d268bafa92
konou0708 <103973949+konou0708@users.noreply.github.com>
2022-12-09 16:20:29 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,3 +1,3 @@
 function imgchange688()
-{document.getElementById('josai').innerHTML='<img src="./image/城西大学マスコットキャラクター.png">';
+{document.getElementById('josai').innerHTML='<img src="./image/abm00035545.png">';
 }
