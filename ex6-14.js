75f56deac95e7262f2027954cb3112df0fc2ebba
konou0708 <103973949+konou0708@users.noreply.github.com>
2023-01-17 09:58:18 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
 function imgchange5() {document.getElementById('logo1').src ="./image/abm00035545.png";}
-function imgchange6() {document.getElementById('logo1').src ="./image/1611144883117.jpg";}
+function imgchange6() {document.getElementById('logo1').src ="./image/城西大学ロゴ.png";}
